036c3e03415775b9fc6b804208e011e0c8e81d75
Una Miralles <unamiralles@hotmail.co.uk>
2020-05-25 15:07:05 +0100

    Added register struct

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,30 @@
 #include <stdlib.h>
+#include <stdint.h>
+
+
+/* Registers stored as 32 bit unsigned ints:
+     0-12  - general use
+     13-14 - SP, LR (ignored for this)
+     15    - PC
+     16    - CPSR
+ */
+
+struct state{
+  uint32_t arr[17]
+}
 
 int main(int argc, char **argv) {
+
+  // Fetch
+  
+  
+
+  // Decode
+
+
+
+  // Execute
+
+  
   return EXIT_SUCCESS;
 }
