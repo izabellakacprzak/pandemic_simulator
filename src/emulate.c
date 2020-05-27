13d3cb86da3abe5cc264671da1339225280d8e5e
Una Miralles <unamiralles@hotmail.co.uk>
2020-05-27 12:03:50 +0100

    Decoding updated, pipeline_utils.h and pipeline_utils.c added

--- src/emulate.c
+++ src/emulate.c
@@ -2,61 +2,13 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <assert.h>
+#include "pipeline_utils.h"
 
-#define MEMORY_SIZE 32768
 #define MASK_16 (1 << 16) - 1
-
-/* Registers stored as 32 bit unsigned ints:
-     0-12  - general use
-     13-14 - SP, LR (ignored for this)
-     15    - PC
-     16    - CPSR
- */
-
-typedef uint32_t instruction;
-
-struct CurrentState{
-  // general purpose registers
-  uint32_t reg0;
-  uint32_t reg1;
-  uint32_t reg2;
-  uint32_t reg3;
-  uint32_t reg4;
-  uint32_t reg5;
-  uint32_t reg6;
-  uint32_t reg7;
-  uint32_t reg8;
-  uint32_t reg9;
-  uint32_t reg10;
-  uint32_t reg11;
-  uint32_t reg12;
-  
-  // SP register (ignored for this exercise)
-  uint32_t reg13;
-  
-  // LR register (ignored for this exercise)
-  uint32_t reg14;
-  
-  // PC register
-  uint32_t regPC;
-  
-  // CPSR register
-  uint32_t regCPSR;
-
-  uint8_t memory[MEMORY_SIZE];
-};
-
-struct Pipeline{
-  instruction fetched;
-  instruction decoded;
-  instruction executed;
-};
-
-enum InstructType {HALT, DATA_PROCESSING, MULTIPLY, SINGLE_DATA_TRANSFER, BRANCH};
 
 int loadToMemory(struct CurrentState currentState, char *filepath){
   FILE *sourceFile;
-  sourceFile = fopen(filepath, "rb")
+  sourceFile = fopen(filepath, "rb");
    
   if(!sourceFile){
     perror("Error opening file");
