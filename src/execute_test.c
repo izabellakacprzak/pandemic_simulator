fa8be4e2cae037adb3416410bc28558b8f0d9958
Matthew Hill <mh1619@ic.ac.uk>
2020-05-27 18:11:56 +0100

    changed function arguments to take struct pointers and removed boolean enablers

--- src/execute_test.c
+++ src/execute_test.c
@@ -4,29 +4,29 @@
 #include "pipeline_utils.h"
 
 
-int execute_halt(Instruction intruction, struct CurrentState state, int perform){
+int execute_halt(Instruction intruction, struct CurrentState *state){
   return 4;
 }
 
-int execute_data_processing(Instruction intruction, struct CurrentState state, int perform) {
+int execute_data_processing(Instruction intruction, struct CurrentState *state) {
   return 2;
 }
 
-int execute_multiply(Instruction intruction, struct CurrentState state, int perform){
+int execute_multiply(Instruction intruction, struct CurrentState *state){
   return 3;
 }
 
-int execute_data_transfer(Instruction intruction, struct CurrentState state, int perform) {
+int execute_data_transfer(Instruction intruction, struct CurrentState *state) {
   return 1;
 }
 
-int execute_branch(Instruction intruction, struct CurrentState state, int perform){
+int execute_branch(Instruction intruction, struct CurrentState *state){
   return 0;
 }
 
-int execute(Instruction instruction, struct CurrentState state, int perform,enum instr_type type) {
+int execute(Instruction instruction, struct CurrentState *state, enum instr_type type) {
   //instruction to be executed, machine state, boolean on whether to execute, instruction type
-  int (*executions[5])(Instruction, struct CurrentState, int);
+  int (*executions[5])(Instruction, struct CurrentState*);
 
   //initialises the array with function pointers for each instruction type
   //based on the ordering of instr_type enum
@@ -36,7 +36,7 @@ int execute(Instruction instruction, struct CurrentState state, int perform,enum
   executions[3] = execute_multiply;
   executions[4] = execute_halt;
   
-  return (executions[type](instruction, state, perform));
+  return (executions[type](instruction, state));
   //executes the function corresponding to the type of instruction by the enum's value
 }
 
@@ -50,7 +50,7 @@ int main(void) {
   
   for(int i = 0; i < 50000; i++) {
     type_select = rand() % 5;
-    if (execute(instruction,state,1,current_type[type_select]) != type_select) {
+    if (execute(instruction,&state,current_type[type_select]) != type_select) {
       printf("TEST FAILED AT INDEX %d\n", i);
       success = 0;
     } 
