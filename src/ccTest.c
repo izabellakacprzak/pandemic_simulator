557358dd2be76f328a1754641c580c0fdf3b98de
Matthew Hill <mh1619@ic.ac.uk>
2020-05-27 11:03:41 +0100

    neatened up the layout

--- src/ccTest.c
+++ src/ccTest.c
@@ -34,6 +34,12 @@ struct CurrentState{
   uint8_t memory[MEMORY_SIZE];
 };
 
+enum cpsrMask {
+  Z = 0b0100,
+  NV = 0b1001,
+  NZV = 0b1101
+};
+
 int cpsrCheck(struct CurrentState state, int mask, int *expected, int no_expected) {
   int res;
   mask = mask << 28;
@@ -47,35 +53,55 @@ int cpsrCheck(struct CurrentState state, int mask, int *expected, int no_expecte
 }
 
 int checkCC(int instruction,struct CurrentState state) {
-  int mask = (2 << 32) - (2 << 28);
+  int mask = (1 << 32) - (1 << 28);
   int masked = instruction & mask;
   int cmp[3];
-  
+  int cmpLength;
+  enum cpsrMask newMask;
+  enum cpsrMask temp = NV;
   switch (masked) {
-  case 0:
-    cmp[0] = 1;
-    return cpsrCheck(state, 4, cmp, 1);
-  case 1:
-    cmp[0] = 0;
-    return cpsrCheck(state, 4, cmp, 1);
-  case 10:
-    cmp[0] = 0;
-    cmp[1] = 2;
-    return cpsrCheck(state, 9, cmp, 2);
-  case 11:
-    cmp[0] = 1;
-    return cpsrCheck(state, 9, cmp, 1);
-  case 12:
-    cmp[0] = 1;
-    cmp[1] = 3;
-    return cpsrCheck(state, 13, cmp, 2);
-  case 13:
-    cmp[0] = 1;
-    cmp[1] = 2;
-    cmp[2] = 3;
-    return cpsrCheck(state, 13, cmp, 3);
+  case 0b0000:
+    cmp[0] = 1; //Z == 1
+    cmpLength = 1;
+    newMask = Z;
+    break;
+  case 0b0001:
+    cmp[0] = 0; //Z == 0
+    newMask = Z;
+    cmpLength = 1;
+    break;
+  case 0b1010:
+    cmp[0] = 0; //N == 0 && V == 0
+    cmp[1] = 2; //N == 1 && V == 1
+    newMask = NV;
+    cmpLength = 2;
+    break;
+  case 0b1011:
+    cmp[0] = 1; //N == 1 && V == 0 || N == 0 && V == 1
+    newMask = NV;
+    cmpLength = 1;
+    break;
+  case 0b1100:
+    cmp[0] = 1; //Z == 1 && N == 0 && V == 0
+    cmp[1] = 3; //Z == 1 && N == 1 && V == 1
+    newMask = NZV;
+    cmpLength = 2;
+    break;
+  case 0b1101:
+    cmp[0] = 1; //Z == 0 && N == 1 && V == 0 || Z == 0 && N == 0 && V == 1 || Z == 1 && N == 0 && V == 0
+    cmp[1] = 2; //Z == 1 && N == 1 && V == 0 || Z == 1 && N == 0 && V == 1
+    cmp[2] = 3; // Z == 1 && N == 1 && V == 1
+    newMask = NZV;
+    cmpLength = 3;
+    if ((state.regCPSR & newMask) == (state.regCPSR & temp)) {
+      return 0; //Z == 0 && N == 1 && V == 1
+    }
+    break;
+  case 0b1110:
+    return 1;
   default:
     perror("ERROR: invalid CPSR code");
     return 0;
   }
+  return cpsrCheck(state,newMask,cmp,cmpLength);
 }
