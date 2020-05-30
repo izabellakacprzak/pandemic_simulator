1a5df5dc705edc2cb7e505ddd66c78f34adffc5c
Nicole <no119@ic.ac.uk>
2020-05-30 03:18:57 -0700

    Combined multiply and data transfer instructions

--- src/lib/execute_utils.c
+++ src/lib/execute_utils.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+#include <stdlib.h>
 #include "execute_utils.h"
 #include "pipeline_utils.h"
 
@@ -11,12 +13,27 @@ int execute_data_processing(Instruction intruction, State *state) {
   return 2;
 }
 
-int execute_multiply(Instruction intruction, State *state){
-  if(determineValidity(instruction, *state){
 
+uint32_t *getRegPointer(int reg, State *currentState, Instruction instruction){
+	uint32_t *regPtr = &currentState->reg0;
+	// getting the 4 bits of the instruction
+	// which correspond to the register which
+	// starts at bit reg
+	int regAddress = instruction & ((1 << (reg + 1)) - 1) >> (reg - 4);
+	regPtr += regAddress;
+	return regPtr;
+}
+
+
+int execute_multiply(Instruction instruction, State *state){
       //get destination register pointer - bits 19 to 16 in instr
+      // Rm - bits 3 to 0
+      // Rs - bits 11 to 8
+      
+      uint32_t *regRd = getRegPointer(19, state, instruction);
+      uint32_t *regRs = getRegPointer(11, state, instruction);
+      uint32_t *regRm = getRegPointer(3, state, instruction);
 
-      uint32_t *destPtr;
       
       // check whether to perform multiply and accumulate or
       // multiply only - function for now in pipeline_utils
@@ -24,35 +41,67 @@ int execute_multiply(Instruction intruction, State *state){
 
 	// set destination register to Rm x Rs + Rn
 	// Rn - bits 15 to 12
-	// Rm - bits 3 to 0
-	// Rs - bits 11 to 8
+
+	uint32_t *regRn = getRegPointer(15, state, instruction);
+
+	*regRd = (*regRm) * (*regRs) + (*regRn);	
 
       } else{
 
 	//set destination register to Rm x Rs
-	// Rm - bits 3 to 0
-	// Rs - bits 11 to 8
+        *regRd = (*regRm) * (*regRs);
 	
       }
 
       if(setCPSR(instruction)){
 
 	//set result to the value in destination register
-	int result;
+	int result = *regRd;
 	setZ(state, result);
 	setN(state, result);
 
       }
 
       // return 1 if successful;
-    }
-
-    //return 0 if unsuccessful;
-  return 3;
+      return 1;
 }
 
-int execute_data_transfer(Instruction intruction, State *state) {
-  return 1;
+int execute_data_transfer(Instruction instruction, State *state) {
+
+  int offset = instruction & ((1 << 12) - 1);
+	// here the function which calculates the offset
+	// depending on the I bit of the instruction
+  
+	uint32_t *destReg = getRegPointer(15, state, instruction);
+	uint32_t *baseReg = getRegPointer(19, state, instruction);
+	
+	if(!getU(instruction)){
+		// subtracting offset
+		offset = -offset;
+	}
+	
+	if(getP(instruction)){
+		// pre-indexing
+		if(getL(instruction)){
+			// loading
+			*destReg = state->memory[*baseReg + offset];
+		} else {
+			// storing
+			state->memory[*baseReg + offset] = *destReg;
+		}
+	} else {
+		// post-indexing
+		if(getL(instruction)){
+			// loading
+			*destReg = state->memory[*baseReg];
+			*baseReg += offset;
+		} else {
+			// storing
+			state->memory[*baseReg] = *destReg;
+			*baseReg += offset;
+		}
+	}
+	return 1;
 }
 
 int execute_branch(Instruction intruction, State *state){
--- src/lib/execute_utils.h
+++ src/lib/execute_utils.h
@@ -2,6 +2,10 @@
 #ifndef EXECUTE_UTILS
 #define EXECUTE_UTILS
 
+// return a pointer to a register whose address
+// is the 4 bits of instruction starting from bit reg
+uint32_t *getRegPointer(int reg, State *currentState, Instruction instruction);
+
 int execute_halt(Instruction intruction, State *state);
 
 int execute_data_processing(Instruction intruction, State *state);
