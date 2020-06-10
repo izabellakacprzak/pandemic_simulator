6bac59155e771687a081cf5ce81ebb00e34fdade
Kacprzak, Izabella <izabella.kacprzak19@imperial.ac.uk>
2020-06-10 10:49:39 +0200

    started implementing set functions, set registers

--- src/lib/assemble_utils.c
+++ src/lib/assemble_utils.c
@@ -95,31 +95,150 @@ void free_node(Dictionary *root) {
   free(root);
 }
 
+// sets bits for instructions that
+// compute results: and, eor, sub, rsb, add, orr
+void withResults(Instruction instruction, char **code){
+	int rd = atoi(strtok(code[1], "r"));
+	int rn = atoi(strtok(code[2], "r"));
+	int operand;
+	if(code[3][0] == '#'){
+		// set the I flag
+		instruction = instruction | (1 << 25);
+
+		// NEED TO ROTATE //
+		if(code[3][2] == 'x'){
+			// hexadecimal value
+			operand = atoi(strtok(code[3], "#0x"));
+		}
+		else{
+			// decimal value
+			operand = atoi(strtok(code[3], "#"));
+		}
+		int rotations = 0;
+		while((operand >> 8) != 0){
+			rotations++;
+			operand = (operand >> 1) | (operand << 32);
+		}
+		instruction = instruction | ((rotations/2) << 8);
+		instruction = instruction | operand;
+	}
+	else{
+
+	}
+
+
+	instruction = instruction | (rd << 12);
+	instruction = instruction | (rn << 16);
+	instruction = instruction | operand;
+}
+
+// sets bits for instructions that
+// do single operand assignment: mov
+void operandAssignment(Instruction instruction, char **code){
+	int rd = atoi(strtok(code[1], "r"));
+	int operand = atoi(code[2]);
+
+	instruction = instruction | (rd << 12);
+	instruction = instruction | operand;
+}
+
+// sets bits for instructions that
+// do not compute results but
+// set the CPSR flags: tst, teq, cmp
+void noResults(Instruction instruction, char **code){
+	int rn = atoi(strtok(code[1], "r"));
+	int operand = atoi(code[2]);
+
+	instruction = instruction | (rn << 16);
+	instruction = instruction | operand;
+
+	instruction = instruction | (1 < 20);
+}
+
 // all of these probably take char *nextInstruction or nothing at all
-static Instruction setDataProcessing() {
+static Instruction setDataProcessing(char **code) {
 
   Instruction instruction = 0;
 
-  //sets Cond Code
-  instruction = setBits(al, 31, instruction);
-
+  //sets condition code
+  instruction = setBits(al, 28, instruction);
+ 
+  // setting the opcode bits
   //if instruction is supposed to be tst, teq or cmp - set S - bit 20
   // otherwise it is clear (cleared upon initialisation)
+  if(!strcmp(code[0], "and")){
+		  withResults(instruction, code);
+  }
+  else if(!strcmp(code[0], "eor")){
+	  withResults(instruction, code);
+	  instruction = instruction | (1 << 21);
+  }
+  else if(!strcmp(code[0], "sub")){
+	  withResults(instruction, code);
+	  instruction = instruction | (2 << 21);
+  }
+  else if(!strcmp(code[0], "rsb")){
+	  withResults(instruction, code);
+	  instruction = instruction | (3 << 21);
+  }
+  else if(!strcmp(code[0], "add")){
+	  withResults(instruction, code);
+	  instruction = instruction | (4 << 21);
+  }
+  else if(!strcmp(code[0], "orr")){
+	  withResults(instruction, code);
+	  instruction = instruction | (12 << 21);
+  }
+  else if(!strcmp(code[0], "mov")){
+	  operandAssignment(instruction, code);
+	  instruction = instruction | (13 << 21);
+  }
+  else if(!strcmp(code[0], "tst")){
+	  noResults(instruction, code);
+	  instruction = instruction | (8 << 21); 
+  }
+  else if(!strcmp(code[0], "teq")){
+	  noResults(instruction, code);
+	  instruction = instruction | (9 << 21);
+  }
+  else if(!strcmp(code[0], "cmp")){
+	  noResults(instruction, code);
+	  instruction = instruction | (10 << 21);
+  }
 
   return instruction;
 }
 
-static Instruction setMultiply() {
+void setRegistersMultiply(Instruction instruction, char **code){
+	int rd = atoi(strtok(code[1], "r"));
+	int rm = atoi(strtok(code[2], "r"));
+	int rs = atoi(strtok(code[3], "r"));
+
+	instruction = instruction | (rd << 16);
+	instruction = instruction | rm;
+	instruction = instruction | (rs << 8);
+}
+
+static Instruction setMultiply(char **code) {
   Instruction instruction = 0;
-  //sets Cond Code
-  instruction = setBits(al, 31, instruction);
-  instruction = setBits(0x9, 7, instruction);
 
-  //check whether mnemonic is mla or mul
-  // and set A accordingly
-  //on initialisation instruction is set with all bits to 0
-  // so there is no need to manually set S
+  // sets condition code
+  instruction = setBits(al, 18, instruction);
+  instruction = setBits(0x9, 4, instruction);
 
+
+  // sets the register bits
+  // and A bit if instructino is mla
+  // there is no need to set S because instruction is initially all 0
+  if(!strcmp(code[0], "mul")){
+	  setRegistersMultiply(instruction, code);
+  }
+  else if(!strcmp(code[0], "mla")){
+	  instruction = instruction | (1 << 21);
+	  setRegistersMultiply(instruction, code);
+	  int rn = atoi(strtok(code[4], "r"));
+	  instruction = instruction | (rn << 12);
+  }
 
   return instruction;
 }
@@ -128,20 +247,41 @@ static Instruction setDataTransfer() {
 
   Instruction instruction = 0;
   
+  // set condition code
   instruction = setBits(1, 26, instruction);
   
+
+
   return 0;
 }
 
-static Instruction setBranch() {
+static Instruction setBranch(char **code) {
 
   Instruction  instruction = 0;
-  //check the mnemonic suffix for b and execute with that condition
-  //as of now set to al(if no suffix - al)
-  //sets Cond Code
-  instruction = setBits(al, 31, instruction);
-  //sets the 101 from 27
-  instruction = setBits(0x5, 27, instruction);
+
+  // set the 101 at bits 27 - 25
+  instruction = setBits(0x5, 25, instruction);
+
+
+  // set condition code
+  if(!strcmp(code[0], "bne")){
+	  instruction = instruction | (1 << 28);
+  }
+  else if(!strcmp(code[0], "bge")){
+	  instruction = instruction | (10 << 28);
+  }
+  else if(!strcmp(code[0], "blt")){
+	  instruction = instruction | (11 << 28);
+  }
+  else if(!strcmp(code[0], "bgt")){
+	  instruction = instruction | (12 << 28);
+  }
+  else if(!strcmp(code[0], "ble")){
+	  instruction = instruction | (13 << 28);
+  }
+  else{
+	  instruction = instruction | (14 << 28);
+  }
 
   return instruction;
 }
