fb7e8cc3380ade0caa9708d38a3f7c3ff29fef1e
Matthew Hill <mh1619@ic.ac.uk>
2020-06-14 18:25:49 +0100

    added the key structs and a skeleton main loop for terminal output

--- extension/lib/simulate_utils.h
+++ extension/lib/simulate_utils.h
@@ -1,1 +1,40 @@
-#include <errno.h>
+
+#ifndef SIMULATE_UTILS
+#define SIMULATE_UTILS
+
+typedef enum healthStatus {
+  HEALTHY,
+  INFECTED,
+  INFECTED_WITH_SYMPTOMS
+} HealthStatus;
+
+typedef enum cellType {
+  NORMAL,
+  WORK,
+  HOSPITAL
+} CellType;
+
+typedef struct humanStruct {
+  int x;
+  int y;
+  //gridCell *cell;
+  int risk; //could be a float based on design later on
+  HealthStatus status;
+} Human;
+
+typedef struct gridCellStruct {
+  Human *human;
+  CellType type;
+} GridCell;
+
+typedef GridCell** Grid;
+
+//sets a cell to be occupied by a given human
+#define CELL_SET(cell, human) \
+  do { cell.human = human; } while (0)
+
+//sets a cell to be unoccupied
+#define CELL_CLEAR(cell) \
+  do { cell.human = NULL; } while (0)
+
+#endif
